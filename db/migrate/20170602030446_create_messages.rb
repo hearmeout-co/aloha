a4e15013c7e23b4fda2eafe8f2348b3e62d9f315
Benjamin Jackson <bhjackson@gmail.com>
2017-06-02 20:00:32 -0400

    Refactoring, renamed column.

--- db/migrate/20170602030446_create_messages.rb
+++ db/migrate/20170602030446_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.text :message
+      t.text :content
       t.string :label, index: true
       t.string :delay
       t.timestamps
