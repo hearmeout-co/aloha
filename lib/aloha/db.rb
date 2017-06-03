46c53c8e4107cfeeed5c8a51951fec63f9702f4c
“Benjamin <bhjackson@gmail.com>
2017-06-02 20:17:36 -0400

    Removed redundant slash from postgres URL.

--- lib/aloha/db.rb
+++ lib/aloha/db.rb
@@ -1,6 +1,6 @@
 require 'sinatra/activerecord'
 
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/aloha')
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/aloha')
 
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
