5b2a91e16ba00af9f367517b479b2acc03dcf2cc
Benjamin Jackson <bhjackson@gmail.com>
2017-06-03 11:43:37 -0400

    Refactoring, extracted message loading into encapsulated hook class.

---
+++ tests/hooks/load_messages_test.rb
@@ -0,0 +1,16 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class LoadMessagesTest < AlohaTest
+  def setup
+    super
+    Aloha::Hooks::LoadMessages.new.call(nil, nil)
+  end
+
+  test 'server creates messages in database on load' do
+    assert(Message.count > 0)
+  end
+
+  test 'server loads messages with data from main config file' do
+    assert(Message.where(label: "basics").exists?)
+  end
+end
--- tests/server_test.rb
+++ tests/server_test.rb
@@ -1,24 +1,6 @@
 require File.expand_path '../test_helper.rb', __FILE__
 
 class ServerTest < AlohaTest
-  def setup
-    super
-    Aloha::Server.load_messages!
-  end
-
-  test 'server creates messages in database on load' do
-    assert(Message.count > 0)
-  end
-
-  test 'server loads messages with data from main config file' do
-    assert(Message.where(label: "basics").exists?)
-  end
-
-  test 'server reports that user is initialized if a record exists with its username' do
-    assert !Aloha::Server.initialized?('ben')
-    user = new_user
-    assert Aloha::Server.initialized?('ben')
-  end
 
   test 'server welcomes new user with team name' do
     setup_client
