5023a4a36bb0ee4baa302068609f5c9a63881a60
Benjamin Jackson <bhjackson@gmail.com>
2017-06-02 22:14:32 -0400

    Added validations and tests for user model.

--- lib/aloha/models/user.rb
+++ lib/aloha/models/user.rb
@@ -2,6 +2,8 @@ require 'chronic'
 
 class User < ActiveRecord::Base
   has_many :deliveries
+  validates :username, presence: true
+  validates :slack_id, presence: true
 
   def ready_for?(message)
     return true if message.delay.blank?
