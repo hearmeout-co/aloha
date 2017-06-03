1ee78aee91995d7bf35723e159daf1cee764096f
Benjamin Jackson <bhjackson@gmail.com>
2017-06-02 20:30:14 -0400

    Added helper methods to user model for message send logic and updated the server to use them instead of the legacy Pstore data.

--- lib/aloha/models/user.rb
+++ lib/aloha/models/user.rb
@@ -1,3 +1,15 @@
+require 'chronic'
+
 class User < ActiveRecord::Base
   has_many :deliveries
+
+  def ready_for?(message)
+    return true if message.delay.blank?
+    time_with_delay = Chronic.parse(message.delay + " ago")
+    self.created_at < time_with_delay
+  end
+
+  def received?(message)
+    Delivery.where(user: self, message: message).exists?
+  end
 end
