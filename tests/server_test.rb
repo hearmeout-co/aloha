03fba8290a4899cce73f5f7357e4bd168a62f1a8
“Benjamin <bhjackson@gmail.com>
2017-06-02 22:29:31 -0400

    Added test for Aloha::Server.initialized?

--- tests/server_test.rb
+++ tests/server_test.rb
@@ -13,4 +13,10 @@ class ServerTest < AlohaTest
   test 'server loads messages with data from main config file' do
     assert(Message.where(label: "basics").exists?)
   end
+
+  test 'server reports that user is initialized if a record exists with its username' do
+    assert !Aloha::Server.initialized?('ben')
+    user = User.create!(username: 'ben', slack_id: 'U024BE7LH')
+    assert Aloha::Server.initialized?('ben')
+  end
 end
