11c96b7fb607782c2c3080bc75926dad54d55f4f
“Benjamin <bhjackson@gmail.com>
2017-06-04 12:27:27 -0400

    Moved base test class inline into test helper.

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -8,8 +8,18 @@ require "mocha/mini_test"
 require File.expand_path '../../app.rb', __FILE__
 
 require 'database_cleaner'
-
-require File.expand_path '../base.rb', __FILE__
-
 DatabaseCleaner.strategy = :truncation
 
+class AlohaTest < ActiveSupport::TestCase
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+
+  def new_user
+    @new_user ||= User.create!(username: 'ben', slack_id: 'U024BE7LH')
+  end
+end
