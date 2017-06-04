ee34f34501352c7a3cf7c29feee512cb58698857
Benjamin Jackson <bhjackson@gmail.com>
2017-06-04 17:19:18 -0400

    Added test to ensure messages with no delay are delivered on welcome.

--- tests/hooks/welcome_new_user_test.rb
+++ tests/hooks/welcome_new_user_test.rb
@@ -6,15 +6,21 @@ class WelcomeNewUserTest < AlohaTest
     user = stub(name: 'ben', id: 'U024BE7LH')
     client.stubs(:users).returns('U024BE7LH' => user)
     client.stubs(:team).returns(stub(name: 'Aloha'))
+    client.web_client.expects(:chat_postMessage).at_least_once
+    @data = stub(user: 'U024BE7LH')
     Aloha::Hooks::LoadMessages.new.call(client, nil)
   end
 
   test 'it welcomes new users with the team name' do
-    client.web_client.expects(:chat_postMessage).at_least_once
     options = { text: "Welcome to Aloha!", channel: "@ben", as_user: true, link_names: true }
     client.web_client.expects(:chat_postMessage).with(options).once
+    Aloha::Hooks::WelcomeNewUser.new.call(client, @data)
+  end
 
-    data = stub(user: 'U024BE7LH')
-    Aloha::Hooks::WelcomeNewUser.new.call(client, data)
+  test 'it welcomes new users with messages that have no delay' do
+    text = "Go ahead and *introduce yourself in the #introductions channel* so we can all get to know who you are! Consider answering:\n\n- What’s your background?\n- What kind of work do you do?\n- How did you hear of the Slack community?\n- What do you hope to get out of our community?"
+    options = { text: text, channel: "@ben", as_user: true, link_names: true }
+    client.web_client.expects(:chat_postMessage).with(options).once
+    Aloha::Hooks::WelcomeNewUser.new.call(client, @data)
   end
 end
