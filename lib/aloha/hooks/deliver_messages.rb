53ac1449173eea8c4febf350028de267b946eb80
“Benjamin <bhjackson@gmail.com>
2017-06-04 17:25:04 -0400

    Refactoring, renamed variables.

--- lib/aloha/hooks/deliver_messages.rb
+++ lib/aloha/hooks/deliver_messages.rb
@@ -1,16 +1,16 @@
 module Aloha
   module Hooks
     class DeliverMessages
-      def call client, message
-        return if message.presence == 'active'
-        return if client.users[message.user].name == client.name
+      def call client, data
+        return if data.presence == 'active'
+        return if client.users[data.user].name == client.name
 
-        user_id = client.users[message.user].id
+        user_id = client.users[data.user].id
         user = User.find_by(slack_id: user_id)
         
         if user
-          Message.all.each do |msg|
-            msg.deliver!(client, user)
+          Message.all.each do |message|
+            message.deliver!(client, user)
           end
         end
       end
