882ff9e1b1bf1c1b63fbdbf377d92e400269cdbc
Benjamin Jackson <bhjackson@gmail.com>
2017-06-06 00:09:59 -0400

    Added refresh command.

--- lib/aloha/bot.rb
+++ lib/aloha/bot.rb
@@ -1,3 +1,22 @@
+require 'aloha/controller'
+
 module Aloha
-  class Bot < SlackRubyBot::Bot; end
+
+  class Bot < SlackRubyBot::Bot
+    help do
+      title 'Aloha Bot'
+      desc 'Aloha helps onboard new team members with scheduled messages.'
+
+      command 'refresh' do
+        desc 'Refresh messages after updating the configuration.'
+      end
+    end
+
+    model = SlackRubyBot::MVC::Model::Base.new
+    view = SlackRubyBot::MVC::View::Base.new
+    @controller = Aloha::Controller.new(model, view)
+    @controller.class.command_class.routes.each do |route|
+      STDERR.puts route.inspect
+    end
+  end
 end
