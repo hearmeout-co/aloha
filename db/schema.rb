9d67686e0ff541ebb8139d39f7c0e9fcfcbc183a
“Benjamin <bhjackson@gmail.com>
2017-06-06 14:51:09 -0400

    Added an is_admin flag to the user model and updated the hooks on "hello" to create the admin user if they don't exist yet and set the flag to true.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602030451) do
+ActiveRecord::Schema.define(version: 20170606162110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170602030451) do
     t.string "slack_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
     t.index ["slack_id"], name: "index_users_on_slack_id"
   end
 
