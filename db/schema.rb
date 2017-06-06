cbc71aaf642ba8389683e0d72cbb0bd71a7ee616
“Benjamin <bhjackson@gmail.com>
2017-06-06 16:52:25 -0400

    Added admin_only flag to message model for selective delivery.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606162110) do
+ActiveRecord::Schema.define(version: 20170606190047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170606162110) do
     t.string "delay"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin_only"
     t.index ["label"], name: "index_messages_on_label"
   end
 
