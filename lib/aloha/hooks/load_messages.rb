34bc9762f22fe2272bbef718b9afaa1c59df845d
“Benjamin <bhjackson@gmail.com>
2017-06-05 23:10:51 -0400

    Refactoring, renamed variables for clarity.

--- lib/aloha/hooks/load_messages.rb
+++ lib/aloha/hooks/load_messages.rb
@@ -1,10 +1,10 @@
 module Aloha
   module Hooks
     class LoadMessages
-      def call client, message
+      def call client, data
         config_file = ENV['MESSAGES_CONFIG_FILE'] || File.join($ROOT_FOLDER, "config/messages.json")
-        data = open(config_file).read
-        messages = JSON::parse(data)
+        json = open(config_file).read
+        messages = JSON::parse(json)
         messages.each do |msg|
           message = Message.where(label: msg["label"]).first_or_initialize
           message.content = msg["text"]
