efcb2d55b271a0e82561daa3268519967ac0e4e5
Benjamin Jackson <bhjackson@gmail.com>
2017-07-07 15:24:47 -0400

    Fixed duplicate record creation in User#find_create_or_update_by_slack_id!.

--- lib/aloha/models/user.rb
+++ lib/aloha/models/user.rb
@@ -1,6 +1,7 @@
 require 'chronic'
 
 class User < ActiveRecord::Base
+  belongs_to :team
   has_many :deliveries
   validates :username, presence: true
   validates :slack_id, presence: true
@@ -16,10 +17,10 @@ class User < ActiveRecord::Base
   end
 
   def self.find_create_or_update_by_slack_id!(client, slack_id)
-    instance = User.where(team_id: client.owner, slack_id: slack_id).first
+    instance = User.where(team: client.owner, slack_id: slack_id).first
     instance_info = Hashie::Mash.new(client.web_client.users_info(user: slack_id)).user
-    instance.update_attributes!(username: instance_info.name) if instance && instance.user_name != instance_info.name
-    instance ||= User.create!(team_id: client.owner, slack_id: slack_id, username: instance_info.name)
+    instance.update_attributes!(username: instance_info.name) if instance && instance.username != instance_info.name
+    instance ||= User.create!(team: client.owner, slack_id: slack_id, username: instance_info.name)
     instance
   end
 end
