d3536b6bbfd73077e31bfc93186c48f72cdd2bed
Benjamin Jackson <bhjackson@gmail.com>
2017-07-06 21:26:58 -0400

    The hooks weren't being loaded by the new server class. Updated the class to force them into the options hash on initialization.

--- lib/aloha/server.rb
+++ lib/aloha/server.rb
@@ -9,6 +9,11 @@ module Aloha
       presence_change: [Aloha::Hooks::DeliverMessages.new, Aloha::Hooks::CheckForUpdates.new]
     }
 
+    def initialize(options)
+      options.merge!(hook_handlers: HOOK_HANDLERS)
+      super(options)
+    end
+
     def self.say client, username, text, options={}
       options.merge!(text: text, channel: "@#{username}", as_user: true, link_names: true)
       client.web_client.chat_postMessage(options)
