5d69fab83a7542cf25855e43df29d9a96660e026
“Benjamin <bhjackson@gmail.com>
2017-07-08 12:03:39 -0400

    Added login helpers for user and team tokens and enforced team login at the welcome page.

--- lib/aloha/web.rb
+++ lib/aloha/web.rb
@@ -5,7 +5,25 @@ require 'aloha/controllers/login'
 
 module Aloha
   class Web < Sinatra::Base
-    set :views, Proc.new { File.join(ENV['ROOT_FOLDER'], "lib", "aloha", "views") }
-    set :public_folder, Proc.new { File.join(ENV['ROOT_FOLDER'], "public") }
+    configure do
+      enable :sessions
+      set :session_secret, ENV['SESSION_SECRET']
+      set :views, Proc.new { File.join(ENV['ROOT_FOLDER'], "lib", "aloha", "views") }
+      set :public_folder, Proc.new { File.join(ENV['ROOT_FOLDER'], "public") }
+    end
+
+    helpers do
+      def require_login!
+        if session[:slack_user_token].nil?
+          redirect '/'
+        end
+      end
+      
+      def require_team_login!
+        if session[:slack_team_token].nil?
+          redirect '/'
+        end
+      end
+    end
   end
 end
