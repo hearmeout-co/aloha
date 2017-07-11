e2f7d3293dd4b5411924aca85aa3e86fe271f5aa
Benjamin Jackson <bhjackson@gmail.com>
2017-07-11 13:30:29 -0400

    Numeric delays for messages are now set in a before_save hook rather than in the controller.

--- lib/aloha/models/message.rb
+++ lib/aloha/models/message.rb
@@ -3,10 +3,18 @@ class Message < ActiveRecord::Base
   has_many :deliveries
   scope :for_users, -> { where(admin_only: false) }
 
+  before_save :set_delay
+
   def deliver! client, user
     if user.ready_for?(self) && !user.received?(self)
       Aloha::Server.say(client, user.username, self.content)
       Delivery.where(message: self, user: user).first_or_create!
     end
+  end
+
+  private
+  def set_delay
+    delay_string = "#{self.delay_value} #{self.delay_type}"
+    self.delay = ChronicDuration.parse(delay_string).to_i
   end
 end
