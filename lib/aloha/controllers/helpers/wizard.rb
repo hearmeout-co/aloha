2d4df53ee898de96115b6e6aa698780eba2383b3
Benjamin Jackson <bhjackson@gmail.com>
2017-07-11 13:02:33 -0400

    Fixed runtime errors and errors in message text when submitting the wizard.

--- lib/aloha/controllers/helpers/wizard.rb
+++ lib/aloha/controllers/helpers/wizard.rb
@@ -2,7 +2,7 @@ module Aloha
   class Web < Sinatra::Base
     helpers do
       def redirect_to_next_step
-        if params[:step] == WIZARD_STEPS.length
+        if params[:step].to_i >= WIZARD_STEPS.length
           save_blank_slate
           redirect '/messages'
         else
@@ -17,18 +17,19 @@ module Aloha
       def create_intro_and_coc_message
         text = ""
         if session[:wizard][:intros]
+          channel_name = session[:wizard][:intros].sub(/^#/, '')
           text += <<EOF
-"Go ahead and *introduce yourself in the #introductions channel* so we can all get to know who you are! Consider answering:
+Go ahead and *introduce yourself in the ##{channel_name} channel* so we can all get to know who you are! Consider answering:
 
 - What’s your background?
 - What kind of work do you do?
 - How did you hear of the Slack community?
-- What do you hope to get out of our community?"
+- What do you hope to get out of our community?
 EOF
         end
         if session[:wizard][:coc_link]
           text += "\n\n" if text.length > 0
-          text += "All participants in the #{current_user.team.name} Slack are required to comply with the following code of conduct: #{params[:coc_link]}"
+          text += "All participants in the #{current_user.team.name} Slack are required to comply with the following code of conduct: #{session[:wizard][:coc_link]}"
         end
 
         if text.length > 0
