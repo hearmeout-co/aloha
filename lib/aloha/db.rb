86be6857293db531d255c5163ea04bb353bf2962
Benjamin Jackson <bhjackson@gmail.com>
2017-07-11 11:01:39 -0400

    Fixed failing rake db:create and db:drop tasks.

--- lib/aloha/db.rb
+++ lib/aloha/db.rb
@@ -1,14 +1,9 @@
 if ENV['RACK_ENV'] == 'test'
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/test')
+  ENV['DATABASE_URL'] ||= 'postgres://localhost/test'
 else
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/aloha')
+  ENV['DATABASE_URL'] ||= 'postgres://localhost/aloha'
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+OTR::ActiveRecord.configure_from_url! ENV['DATABASE_URL']
+
+use OTR::ActiveRecord::ConnectionManagement
