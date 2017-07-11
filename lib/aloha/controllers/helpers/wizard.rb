3a565cdc5c0968c149e8c7d645f57b7c1b1dbc3a
“Benjamin <bhjackson@gmail.com>
2017-07-11 13:31:07 -0400

    Added the rest of the steps to the wizard's message generation step.

--- lib/aloha/controllers/helpers/wizard.rb
+++ lib/aloha/controllers/helpers/wizard.rb
@@ -12,13 +12,16 @@ module Aloha
 
       def save_blank_slate
         create_intro_and_coc_message
+        create_wiki_message
+        create_guidance_message
+        create_interruptions_message
       end
 
       def create_intro_and_coc_message
         text = ""
         if session[:wizard][:intros].to_s.length > 0
           channel_name = session[:wizard][:intros].sub(/^#/, '')
-          text += "Go ahead and *introduce yourself in the ##{channel_name} channel* so we can all get to know who you are! Consider answering:
+          text += "Go ahead and *introduce yourself in the `##{channel_name}` channel* so we can all get to know who you are! Consider answering:
 
 - What’s your background?
 - What kind of work do you do?
@@ -32,6 +35,30 @@ module Aloha
 
         if text.length > 0
           message = Message.create!(team: current_user.team, content: text)
+        end
+      end
+
+      def create_wiki_message
+        if session[:wizard][:wiki_link].to_s.length > 0
+          text = "Here's a link to some docs that will help you get started: #{session[:wizard][:wiki_link]}"
+          message = Message.create!(team: current_user.team, content: text, delay_value: 15, delay_type: "minutes")
+        end
+      end
+
+      def create_guidance_message
+        if session[:wizard][:guidance].to_s.length > 0
+          text = guidance
+          message = Message.create!(team: current_user.team, content: text, delay_value: 1, delay_type: "hours")
+        end
+      end
+
+      def create_interruptions_message
+        if session[:wizard][:interruptions] == "on"
+          text = "We do our best to cut down on interruptions:
+
+- Please avoid using `@channel` mentions whenever possible (you can use `@here` instead to notify desktop users).
+- If you need someone's attention, it's usually better to *mention them in a public channel* instead of sending a private message whenever possible."
+          message = Message.create!(team: current_user.team, content: text, delay_value: 1, delay_type: "hours")
         end
       end
     end
