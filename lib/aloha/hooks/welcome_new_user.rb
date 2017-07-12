fd17b200cd219a50b81dc10f1074e67f9c07d623
Benjamin Jackson <bhjackson@gmail.com>
2017-07-12 12:37:30 -0400

    Fixed nil pointer errors caused by slightly different format for user info on team.join event. Fixes #6.

--- lib/aloha/hooks/welcome_new_user.rb
+++ lib/aloha/hooks/welcome_new_user.rb
@@ -1,8 +1,9 @@
 module Aloha
   module Hooks
     class WelcomeNewUser
+      # API docs: https://api.slack.com/events/team_join
       def call client, data
-        user = ::User.find_create_or_update_by_slack_id!(client, data.user)
+        user = ::User.find_create_or_update_by_slack_id!(client, data.user.id)
         
         Aloha::Server.say(client, user.username, "Welcome to #{client.team.name}!")
         Message.all.each do |message|
