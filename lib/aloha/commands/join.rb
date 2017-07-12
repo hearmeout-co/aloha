84b3c2994d961606684f0b40a3e9b9dcc9c6dca4
Benjamin Jackson <bhjackson@gmail.com>
2017-07-12 14:03:15 -0400

    Fixed the "join" command, which regressed during the last fix to the team_join command.

--- lib/aloha/commands/join.rb
+++ lib/aloha/commands/join.rb
@@ -2,9 +2,11 @@ module Aloha
   module Commands
     class Join < SlackRubyBot::Commands::Base
       def self.call(client, data, _match)
-        user = ::User.find_by(slack_id: data.user)
+        slack_id = data.user
+        user = ::User.find_by(slack_id: slack_id)
         if user.nil?
-          Aloha::Hooks::WelcomeNewUser.new.call(client, data)
+          user = User.find_create_or_update_by_slack_id!(client, slack_id)
+          Aloha::Hooks::WelcomeNewUser.new.send_welcome(client, user)
         else
           Aloha::Server.say(client, user.username, "It looks like you've already joined! You'll hear from me eventually.")
         end
