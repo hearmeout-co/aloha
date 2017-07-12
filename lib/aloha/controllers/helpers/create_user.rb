f4144b23251805427754c110543f044e1f0db87b
“Benjamin <bhjackson@gmail.com>
2017-07-12 18:01:59 -0400

    Removed unnecessary call to users.identity that was causing runtime exceptions during installation on teams other than the development team due to scoping issues.

--- lib/aloha/controllers/helpers/create_user.rb
+++ lib/aloha/controllers/helpers/create_user.rb
@@ -1,12 +1,8 @@
 module Aloha
   class Web < Sinatra::Base
     helpers do
-      def create_and_store_logged_in_user(token)
+      def create_and_store_logged_in_user(token, slack_id, team_id)
         user_client = Slack::Web::Client.new(token: token)
-
-        identity = user_client.users_identity
-        team_id = identity['team']['id']
-        user_id = identity['user']['id']
 
         team = Team.where(team_id: team_id).first
         if team && !team.active?
@@ -17,7 +13,7 @@ module Aloha
           redirect '/'
         else
           rt_client = Slack::RealTime::Client.new(token: team.token)
-          user = User.find_create_or_update_by_slack_id!(rt_client, user_id, team)
+          user = User.find_create_or_update_by_slack_id!(rt_client, slack_id, team)
           user.update_attributes!(token: token)
           session[:slack_user_token] = token
           return user
