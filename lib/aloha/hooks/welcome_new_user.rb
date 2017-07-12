13a963b01e2a73adc88b9cb22df919a2d0cb5572
“Benjamin <bhjackson@gmail.com>
2017-07-12 14:03:15 -0400

    Fixed the "join" command, which regressed during the last fix to the team_join command.

--- lib/aloha/hooks/welcome_new_user.rb
+++ lib/aloha/hooks/welcome_new_user.rb
@@ -4,7 +4,10 @@ module Aloha
       # API docs: https://api.slack.com/events/team_join
       def call client, data
         user = ::User.find_create_or_update_by_slack_id!(client, data.user.id)
-        
+        send_welcome(client, user)
+      end
+
+      def send_welcome(client, user)
         Aloha::Server.say(client, user.username, "Welcome to #{client.team.name}!")
         Message.all.each do |message|
           message.deliver!(client, user)
