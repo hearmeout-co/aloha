87d7da0d4089d50d1a5d717be6cf159f7cfff9be
“Benjamin <bhjackson@gmail.com>
2017-07-17 17:22:17 -0400

    Emojis are now displayed correctly in the message list.

--- lib/aloha/presenters/message_presenter.rb
+++ lib/aloha/presenters/message_presenter.rb
@@ -23,7 +23,7 @@ module Aloha
       end
 
       def content_as_html(message)
-        @slack_processor ||= SlackMarkdown::Processor.new(asset_root: '/')
+        @slack_processor ||= SlackMarkdown::Processor.new(asset_root: '/assets')
         @slack_processor.call(message.content)[:output].to_s
       end
     end
