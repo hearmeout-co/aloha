33bc9a414ced9ff84cbd2240fff00550ac1ad2b8
“Benjamin <bhjackson@gmail.com>
2017-07-17 13:39:46 -0400

    Updated Puma config to set pool size based on SINATRA_MAX_THREADS.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,8 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['SINATRA_MAX_THREADS'] || 5)
+ENV['WEB_CONCURRENCY'] ||= "1"
+ENV['SINATRA_MAX_THREADS'] ||= "1"
+
+workers Integer(ENV['WEB_CONCURRENCY'])
+threads_count = Integer(ENV['SINATRA_MAX_THREADS'])
 threads threads_count, threads_count
 preload_app!
 
@@ -10,7 +13,11 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[ENV['RACK_ENV']]
+    config['pool'] = ENV['SINATRA_MAX_THREADS'].to_i
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
 
 before_fork do
