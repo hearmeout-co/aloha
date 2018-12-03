a440b9de2f7311b8eeecdfd02cf2a1f436e4346e
Benjamin Jackson <bhjackson@gmail.com>
2018-12-03 17:16:15 -0500

    Updated RequestPresenceSubscription hook to limit the number of users to 500 and avoid triggering Slack's request limits.

--- lib/aloha/hooks/request_presence_subscriptions.rb
+++ lib/aloha/hooks/request_presence_subscriptions.rb
@@ -3,10 +3,7 @@ module Aloha
     class RequestPresenceSubscriptions < Aloha::Hooks::Base
       def invoke client, data
         team = Team.find_by(team_id: client.team.id)
-        ids = []
-        team.users.find_in_batches(batch_size: 100) do |batch|
-          ids += batch.pluck(:slack_id)
-        end
+        ids = team.users.order('created_at DESC').limit(500).pluck(:slack_id)
         
         # subscribe to presence events for all users
         client.send("send_json", {type: 'presence_sub', ids: ids})
